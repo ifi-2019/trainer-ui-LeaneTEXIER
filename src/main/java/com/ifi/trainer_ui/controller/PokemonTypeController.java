d7759fcd0c57d4101d93f3ffe47e620993afe387
Leane <texierl@b11p12.fil.univ-lille1.fr>
2019-11-25 11:22:14 +0100

    [Add] Travail cours TP5 jusqu'à la question 2 de pour aller plus loin compris

---
+++ src/main/java/com/ifi/trainer_ui/controller/PokemonTypeController.java
@@ -0,0 +1,26 @@
+package com.ifi.trainer_ui.controller;
+
+import com.ifi.trainer_ui.pokemonTypes.service.PokemonTypeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class PokemonTypeController {
+
+    private PokemonTypeService pokemonTypeService;
+
+    @Autowired
+    public void setPokemonTypeService(PokemonTypeService pokemonTypeService) {
+        this.pokemonTypeService = pokemonTypeService;
+    }
+
+    @GetMapping("/pokedex")
+    public ModelAndView pokedex(){
+        ModelAndView modelAndView = new ModelAndView("pokedex");
+        modelAndView.addObject("pokemonTypes", pokemonTypeService.listPokemonsTypes());
+        return modelAndView;
+    }
+}
+
---
+++ src/main/java/com/ifi/trainer_ui/pokemonTypes/service/PokemonTypeServiceImpl.java
@@ -0,0 +1,34 @@
+package com.ifi.trainer_ui.pokemonTypes.service;
+
+import com.ifi.trainer_ui.pokemonTypes.bo.PokemonType;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class PokemonTypeServiceImpl implements PokemonTypeService {
+
+    private RestTemplate restTemplate;
+    private String pokemonServiceUrl;
+
+    public List<PokemonType> listPokemonsTypes() {
+        var url = pokemonServiceUrl + "/pokemon-types/";
+        var listPokemons = Arrays.asList(restTemplate.getForObject(url, PokemonType[].class));
+        listPokemons.sort((a,b) -> a.getId() - b.getId());
+        return listPokemons;
+    }
+
+    @Autowired
+    void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Value("${pokemonType.service.url}")
+    void setPokemonTypeServiceUrl(String pokemonServiceUrl) {
+        this.pokemonServiceUrl = pokemonServiceUrl;
+    }
+}
