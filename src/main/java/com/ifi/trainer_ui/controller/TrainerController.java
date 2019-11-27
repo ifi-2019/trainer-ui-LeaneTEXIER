323f7c0fe893fc92b96807e5b50bc931d5fd2583
Leane <texierl@b11p12.fil.univ-lille1.fr>
2019-11-27 19:16:34 +0100

    [Add] Ajout de l'affichage des trainers

---
+++ src/main/java/com/ifi/trainer_ui/controller/TrainerController.java
@@ -0,0 +1,26 @@
+package com.ifi.trainer_ui.controller;
+
+import com.ifi.trainer_ui.trainers.service.TrainerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class TrainerController {
+
+    private TrainerService trainerService;
+
+    @Autowired
+    public void setTrainerService(TrainerService trainerService) {
+        this.trainerService = trainerService;
+    }
+
+    @GetMapping("/trainers")
+    ModelAndView trainers(){
+        var modelAndView = new ModelAndView("trainers");
+        modelAndView.addObject("trainers", trainerService.listTrainers());
+        return modelAndView;
+    }
+
+}
---
+++ src/main/java/com/ifi/trainer_ui/trainers/service/TrainerServiceImpl.java
@@ -0,0 +1,35 @@
+package com.ifi.trainer_ui.trainers.service;
+
+import com.ifi.trainer_ui.pokemonTypes.bo.PokemonType;
+import com.ifi.trainer_ui.pokemonTypes.service.PokemonTypeService;
+import com.ifi.trainer_ui.trainers.bo.Trainer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Arrays;
+import java.util.List;
+
+@Service
+public class TrainerServiceImpl implements TrainerService {
+
+    private RestTemplate restTemplate;
+    private String trainerServiceUrl;
+
+    public List<Trainer> listTrainers(){
+        var url = trainerServiceUrl + "/trainers/";
+        var listTrainers = Arrays.asList(restTemplate.getForObject(url, Trainer[].class));
+        return listTrainers;
+    }
+
+    @Autowired
+    void setRestTemplate(RestTemplate restTemplate) {
+        this.restTemplate = restTemplate;
+    }
+
+    @Value("${trainer.service.url}")
+    void setTrainerServiceUrl(String trainerServiceUrl) {
+        this.trainerServiceUrl = trainerServiceUrl;
+    }
+}
