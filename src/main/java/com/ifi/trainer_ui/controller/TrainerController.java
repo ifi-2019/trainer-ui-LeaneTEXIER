8265bc92c1a3ca52eb41df5e1aefdc90d1d07347
Leane <texierl@b11p12.fil.univ-lille1.fr>
2019-12-09 11:52:49 +0100

    [Add] Vu des pokemons de la personne connectée

--- src/main/java/com/ifi/trainer_ui/controller/TrainerController.java
+++ src/main/java/com/ifi/trainer_ui/controller/TrainerController.java
@@ -2,10 +2,14 @@ package com.ifi.trainer_ui.controller;
 
 import com.ifi.trainer_ui.trainers.service.TrainerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
+
+import java.security.Principal;
 
 @Controller
 public class TrainerController {
@@ -28,6 +32,15 @@ public class TrainerController {
     ModelAndView trainers(@PathVariable String name){
         var modelAndView = new ModelAndView("trainer");
         modelAndView.addObject("trainer", trainerService.getTrainer(name));
+        return modelAndView;
+    }
+
+
+    @GetMapping("/profile")
+    ModelAndView profile(){
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        var modelAndView = new ModelAndView("profile");
+        modelAndView.addObject("profile", trainerService.getTrainer(auth.getName()));
         return modelAndView;
     }
 
