1df60f2942c55332bac72a19b8a71187b9941c53
Leane <texierl@b11p12.fil.univ-lille1.fr>
2019-12-11 11:55:18 +0100

    [Update] pokedex div

--- src/test/java/com/ifi/trainer_ui/controller/TrainerControllerTest.java
+++ src/test/java/com/ifi/trainer_ui/controller/TrainerControllerTest.java
@@ -3,15 +3,9 @@ package com.ifi.trainer_ui.controller;
 import com.ifi.trainer_ui.trainers.bo.Trainer;
 import com.ifi.trainer_ui.trainers.service.TrainerService;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-
-import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
@@ -22,25 +16,6 @@ class TrainerControllerTest {
     void controllerShouldBeAnnotated(){
         assertNotNull(TrainerController.class.getAnnotation(Controller.class));
     }
-
-//    @Test
-//    void trainers_shouldReturnAModelAndView() {
-//        var trainerService = mock(TrainerService.class);
-//
-//        when(trainerService.listTrainers("Misty")).thenReturn(List.of(new Trainer(), new Trainer()));
-//
-//        Authentication auth = mock(Authentication.class);
-//        when(auth.getName()).thenReturn("Misty");
-//
-//        TrainerController trainerController = new TrainerController();
-//        trainerController.setTrainerService(trainerService);
-//        var modelAndView = trainerController.trainers();
-//
-//        assertEquals("trainers", modelAndView.getViewName());
-//        var trainers = (List<Trainer>)modelAndView.getModel().get("trainers");
-//        assertEquals(2, trainers.size());
-//        verify(trainerService).listTrainers("");
-//    }
 
     @Test
     void trainers_shouldBeAnnotated() throws NoSuchMethodException {
@@ -80,22 +55,6 @@ class TrainerControllerTest {
 
         assertNotNull(pathVariableAnnotation);
     }
-
-//    @Test
-//    void profile_shouldReturnAModelAndView() {
-//        var trainerService = mock(TrainerService.class);
-//
-//        when(trainerService.getTrainer("Misty")).thenReturn(new Trainer("Misty"));
-//
-//        var trainerController = new TrainerController();
-//        trainerController.setTrainerService(trainerService);
-//        var modelAndView = trainerController.profile();
-//
-//        assertEquals("profile", modelAndView.getViewName());
-//        var trainer = (Trainer)modelAndView.getModel().get("profile");
-//        assertEquals("Misty", trainer.getName());
-//        verify(trainerService).getTrainer("Misty");
-//    }
 
     @Test
     void profile_shouldBeAnnotated() throws NoSuchMethodException {
